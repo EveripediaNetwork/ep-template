cbcd1053e0d09fb162a72185e45eee2196deebfe
Srujan Gurram <srujangs8@gmail.com>
2023-01-31 18:48:31 +0530

    added infura id to config and env example

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ const config = {
   isProduction: process.env.NEXT_PUBLIC_IS_PRODUCTION || false,
   alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
   alchemyChain: process.env.NEXT_PUBLIC_ALCHEMY_CHAIN || 'maticmum',
+  infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
   chainId: process.env.NEXT_PUBLIC_CHAIN_ID || '80001',
   chainName: process.env.NEXT_PUBLIC_CHAIN_NAME || 'Mumbai',
   ensRPC: String(process.env.NEXT_PUBLIC_ENS_RPC),
